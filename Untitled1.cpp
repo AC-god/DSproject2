9ba344dcc7c282acf9a3076ccfa17dba32e6703a
AC-god <51225338+AC-god@users.noreply.github.com>
2019-11-21 22:39:36 +0800

    final

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
-
+ 
 using namespace std;
 ofstream fout("final.path");
 ofstream fout1("test.path");
@@ -23,211 +23,218 @@ public:
         first = a.first;
         second = a.second;
     }
-
+ 
 };
 mypair make_mypair(int a, int b)
 {
     return mypair(a, b);
 }
 mypair startpos, curr;
-
-
+ 
+ 
 template <class T>
 class MyVector
 {
-
+ 
 public:
-
+ 
     MyVector(): size_(0) {}
-
+ 
     MyVector(const MyVector &rhs) = delete;
-
+ 
     explicit MyVector(int n)
     {
         data_ = new T[n];
         size_ = n;
     }
-
+ 
     ~MyVector()
     {
         delete [] data_;
     }
-
+ 
+    void myClear()
+    {
+        delete [] data_;
+        size_ = 0;
+        data_ = new T[3];
+    }
+ 
     int Size() const
     {
         return size_;
     }
-
+ 
     void Resize(int n);
-
+ 
     T &At(int i)
     {
         return data_[i];
     }
-
+ 
     T At(int i) const
     {
         return data_[i];
     }
-
+ 
     bool Empty() const
     {
         if(size_ == 0)
             return true;
         return false;
     }
-
+ 
     T Back()
     {
-
+ 
         return data_[size_ - 1];
     }
     T Front()
     {
-
+ 
         return data_[0];
     }
-
-
+ 
+ 
     void Insert(int pos, int elem)
     {
-
+ 
         MyVector TempMyVector(this->size_);
-
+ 
         for (int i = 0; i < this->size_; ++i)
             TempMyVector.data_[i] = this->data_[i];
-
+ 
         delete [] this->data_;
-
+ 
         size_++;
-
+ 
         data_ = new T[size_];
-
+ 
         for (int i = 0; i < pos; ++i)
             data_[i] = TempMyVector.data_[i];
-
+ 
         this->At(pos) = elem;
-
+ 
         for (int i = pos+1; i < size_; ++i)
             data_[i] = TempMyVector.data_[i-1];
-
+ 
     }
-
+ 
     void Erase(int pos)
     {
-
+ 
         MyVector TempMyVector(this->size_);
-
+ 
         for (int i = 0; i < this->size_; ++i)
             TempMyVector.data_[i] = this->data_[i];
-
+ 
         delete [] this->data_;
-
+ 
         size_--;
-
+ 
         data_ = new T[size_];
-
+ 
         for (int i = 0; i < pos; ++i)
             data_[i] = TempMyVector.data_[i];
-
+ 
         for (int i = pos; i < size_; ++i)
             data_[i] = TempMyVector.data_[i+1];
-
+ 
     }
-
+ 
     void PushFront(T elem)
     {
-
+ 
         MyVector TempMyVector(this->size_);
-
+ 
         for (int i = 0; i < this->size_; ++i)
             TempMyVector.data_[i] = this->data_[i];
-
+ 
         delete [] this->data_;
-
+ 
         size_++;
-
+ 
         data_ = new T[size_];
-
+ 
         for (int i = 0; i < size_-1; ++i)
             data_[i+1] = TempMyVector.data_[i];
-
+ 
         this->At(0) = elem;
-
+ 
     }
-
+ 
     void PopFront()
     {
-
+ 
         MyVector TempMyVector(this->size_);
-
+ 
         for (int i = 0; i < this->size_; ++i)
             TempMyVector.data_[i] = this->data_[i];
-
+ 
         delete [] this->data_;
-
+ 
         size_--;
-
+ 
         data_ = new T[size_];
-
+ 
         for (int i = 0; i < size_; ++i)
             data_[i] = TempMyVector.data_[i+1];
-
+ 
     }
-
+ 
     void Clear()
     {
-
+ 
         for (int i = 0; i < this->size_; ++i)
             data_[i] = 0;
-
+ 
     }
-
+ 
     void PushBack(T elem)
     {
-
+ 
         MyVector TempMyVector(this->size_);
-
+ 
         for (int i = 0; i < this->size_; ++i)
             TempMyVector.data_[i] = this->data_[i];
-
+ 
         delete [] this->data_;
-
+ 
         size_++;
-
+ 
         data_ = new T[size_];
-
+ 
         for (int i = 0; i < size_-1; ++i)
             data_[i] = TempMyVector.data_[i];
-
+ 
         this->At(size_-1) = elem;
-
+ 
     }
-
+ 
     void PopBack()
     {
-
+ 
         MyVector TempMyVector(this->size_);
-
+ 
         for (int i = 0; i < this->size_; ++i)
             TempMyVector.data_[i] = this->data_[i];
-
+ 
         delete [] this->data_;
-
+ 
         size_--;
-
+ 
         data_ = new T[size_];
-
+ 
         for (int i = 0; i < size_; ++i)
             data_[i] = TempMyVector.data_[i];
-
+ 
     }
-
+ 
     T &operator[](int i)
     {
         return At(i);
     }
-
+ 
     T operator[](int i) const
     {
         return At(i);
@@ -235,19 +242,16 @@ public:
 private:
     int size_;
     T *data_;
-
+ 
 };
-
-#include<iostream>
-using namespace std;
-
+ 
 template <class T>
 class MyQueue
 {
 private:
     int capacity, front, back;
     T *arrayqueue;
-
+ 
 public:
     MyQueue():capacity(5),front(-1),back(-1)
     {
@@ -262,15 +266,15 @@ public:
     T Back();
     int getSize();
     int getCapacity();
-
+ 
 };
 template <class T>
 void MyQueue<T>::DoubleCapacity()
 {
-
+ 
     capacity *= 2;
     T *newArrayqueue = new T[capacity];
-
+ 
     int j = -1;
     for (int i = front+1; i <= back; i++)
     {
@@ -285,7 +289,7 @@ void MyQueue<T>::DoubleCapacity()
 template <class T>
 void MyQueue<T>::Push(T x)
 {
-
+ 
     if (IsFull())
     {
         DoubleCapacity();
@@ -295,7 +299,7 @@ void MyQueue<T>::Push(T x)
 template <class T>
 void MyQueue<T>::Pop()
 {
-
+ 
     if (Empty())
     {
         std::cout << "Arrayqueue is empty.\n";
@@ -306,79 +310,60 @@ void MyQueue<T>::Pop()
 template <class T>
 bool MyQueue<T>::IsFull()
 {
-
+ 
     return (back + 1 == capacity);
 }
 template <class T>
 bool MyQueue<T>::Empty()
 {
-
+ 
     return (front  == back);
 }
 template <class T>
 T MyQueue<T>::Front()
 {
-
+ 
     if (Empty())
     {
         std::cout << "Arrayqueue is empty.\n";
         return startpos;
     }
-
+ 
     return arrayqueue[front+1];
 }
 template <class T>
 T MyQueue<T>::Back()
 {
-
+ 
     if (Empty())
     {
         std::cout << "Arrayqueue is empty.\n";
         return -1;
     }
-
+ 
     return arrayqueue[back];
 }
 template <class T>
 int MyQueue<T>::getSize()
 {
-
+ 
     return (back - front);
 }
 template <class T>
 int MyQueue<T>::getCapacity()
 {
-
+ 
     return capacity;
 }
-
+ 
 ifstream fin("floor.data");
 int house[1100][1100] = {0};
 int isclean[1100][1100] = {0};
-int disDFS[1100][1100] = {0};
 int disBFS[1100][1100] = {0};
-bool isvisit[1100][1100] = {0};
-int row, col, battery, maxstep, leaststep;
+int row, col, battery, maxstep, leaststep, numtoclean = 0;
+mypair suggest_closest;
 MyVector<mypair> path;
-MyVector<mypair> trueans;
-
-void makedisDFS(int step, int r, int c)
-{
-    disDFS[r][c] = step;
-    isvisit[r][c] = true;
-    if(r + 1 <= row && !isvisit[r + 1][c] && house[r + 1][c] == 0)
-        makedisDFS(step + 1, r + 1, c);
-
-    if(r - 1 >= 1 && !isvisit[r - 1][c] && house[r - 1][c] == 0)
-        makedisDFS(step + 1, r - 1, c);
-
-    if(c + 1 <= col && !isvisit[r][c + 1] && house[r][c + 1] == 0)
-        makedisDFS(step + 1, r, c + 1);
-
-    if(c - 1 >= 1 && !isvisit[r][c - 1] && house[r][c - 1] == 0)
-        makedisDFS(step + 1, r, c - 1);
-}
-
+ 
 void makedisBFS()
 {
     bool visited[1100][1100] = {0};
@@ -390,7 +375,7 @@ void makedisBFS()
         pos_dis(mypair p = make_mypair(0, 0), int s = 0):pos{p}, step{s} {}
     };
     MyQueue<pos_dis> q;
-
+ 
     q.Push(pos_dis(startpos, 0));
     visited[startpos.first][startpos.second] = true;
     while(!q.Empty())
@@ -427,7 +412,7 @@ void makedisBFS()
 }
 bool check_clean()
 {
-    for(int i = 1; i <= row; i++)
+    /*for(int i = 1; i <= row; i++)
     {
         for(int j = 1; j <= col; j++)
         {
@@ -435,26 +420,110 @@ bool check_clean()
                 return false;
         }
     }
+    return true;*/
+    bool visited[1100][1100] = {0};
+ 
+    MyQueue<mypair> q;
+ 
+    q.Push(startpos);
+    visited[startpos.first][startpos.second] = true;
+    while(!q.Empty())
+    {
+        int r = q.Front().first;
+        int c = q.Front().second;
+        if(isclean[r][c] == 0)
+        {
+            suggest_closest = make_mypair(r, c);
+            return false;
+        }
+        q.Pop();
+        if(r + 1 <= row && !visited[r + 1][c] && house[r + 1][c] == 0)
+        {
+            q.Push(make_mypair(r + 1, c));
+            visited[r + 1][c] = true;
+        }
+        if(r - 1 >= 1 && !visited[r - 1][c] && house[r - 1][c] == 0)
+        {
+            q.Push(make_mypair(r - 1, c));
+            visited[r - 1][c] = true;
+        }
+        if(c + 1 <= col && !visited[r][c + 1] && house[r][c + 1] == 0)
+        {
+            q.Push(make_mypair(r, c + 1));
+            visited[r][c + 1] = true;
+        }
+        if(c - 1 >= 1 && !visited[r][c - 1] && house[r][c - 1] == 0)
+        {
+            q.Push(make_mypair(r, c - 1));
+            visited[r][c - 1] = true;
+        }
+    }
     return true;
 }
 bool isfind = false;
 bool fuck[1100][1100] = {0};
 int disgraph[1100][1100] = {0};
+mypair lastdes = make_mypair(row/2, col/2);
 void findpath(mypair point, mypair des)
 {
-
+ 
     //cout << "!!";
+    if(point == des)
+        return;
     if(!(des == startpos))
     {
-        for(int i = 1; i <= row; i++)
+        MyQueue<mypair> a;
+        bool **tvisited = new bool*[1020];
+        for (int i = 0; i < 1020; i++)
         {
-            for(int j = 1; j <= col; j++)
-            {
-                disgraph[i][j] = 99999;
-            }
+            tvisited[i] = new bool[1020] {};
         }
+        tvisited[lastdes.first][lastdes.second] = true;
+        while(!a.Empty())
+        {
+            int r = a.Front().first;
+            int c = a.Front().second;
+            if(disgraph[r][c] == 99999)
+                break;
+            else
+                disgraph[r][c] = 99999;
+            a.Pop();
+            if(r + 1 <= row && !tvisited[r + 1][c])
+            {
+                a.Push(make_mypair(r + 1, c));
+                tvisited[r + 1][c] = true;
+            }
+            if(r - 1 >= 1 && !tvisited[r - 1][c])
+            {
+                a.Push(make_mypair(r - 1, c));
+                tvisited[r - 1][c] = true;
+            }
+            if(c + 1 <= col && !tvisited[r][c + 1])
+            {
+                a.Push(make_mypair(r, c + 1));
+                tvisited[r][c + 1] = true;
+            }
+            if(c - 1 >= 1 && !tvisited[r][c - 1])
+            {
+                a.Push(make_mypair(r, c - 1));
+                tvisited[r][c - 1] = true;
+            }
+ 
+        }
+        for(int i = 0; i < 1020; i++)
+        {
+            delete [] tvisited[i];
+        }
+        delete [] tvisited;
+        //cout << "###";
+        lastdes = des;
         disgraph[des.first][des.second] = 0;
-        bool visited[1100][1100] = {0};
+        //bool visited[1100][1100] = {0};
+        bool **visited = new bool*[1020];
+        for (int i = 0; i < 1020; i++)
+        {
+            visited[i] = new bool[1020] {};
+        }
         //cout << des.first << "%%%" << des.second << endl;
         class pos_dis
         {
@@ -464,7 +533,7 @@ void findpath(mypair point, mypair des)
             pos_dis(mypair p = make_mypair(0, 0), int s = 0):pos{p}, step{s} {}
         };
         MyQueue<pos_dis> q;
-
+ 
         q.Push(pos_dis(des, 0));
         visited[des.first][des.second] = true;
         while(!q.Empty())
@@ -475,7 +544,6 @@ void findpath(mypair point, mypair des)
             //cout << r << " " << c << " " << point.first << " " << point.second << endl;
             if(r == point.first && c == point.second)
             {
-
                 break;
             }
             int step = q.Front().step;
@@ -505,6 +573,11 @@ void findpath(mypair point, mypair des)
                 visited[r][c - 1] = true;
             }
         }
+        for(int i = 0; i < 1020; i++)
+        {
+            delete [] visited[i];
+        }
+        delete [] visited;
         //cout << "??????????";
         int stepcost = disgraph[point.first][point.second];
         path.PushBack(point);
@@ -600,30 +673,26 @@ void findpath(mypair point, mypair des)
             }
         }
     }
-
+ 
     //cout  << path.Back().first << " " << path.Back().second << endl;
 }
-
-void resetfuck()
-{
-    for(int i = 1; i <= row; i++)
-    {
-        for(int j = 1; j <= col; j++)
-        {
-            fuck[i][j] = false;
-        }
-    }
-}
+ 
 void showpath()
 {
-    for(int i = 1; i < path.Size(); i++)
+    int sss = path.Size();
+    for(int i = 1; i < sss; i++)
     {
         //trueans.PushBack(path[i]);
         fout1 << path[i].first << " " << path[i].second << endl;
-        count++;
-        if(!(path.Back() == startpos) && leaststep < battery / 2)
+        if(isclean[path[i].first][path[i].second] == 0)
         {
-
+            isclean[path[i].first][path[i].second] = 1;
+            numtoclean--;
+        }
+        count++;
+        if(leaststep < battery / 2)
+        {
+ 
             if(house[path[i].first + 1][path[i].second] == -1)
             {
                 //trueans.PushBack(startpos);
@@ -665,61 +734,48 @@ void showpath()
                 break;
             }
         }
-
-
+ 
+ 
         //cout << path[i].first << " " << path[i].second << endl;
     }
-
+ 
 }
-void clearpath()
-{
-    while(!path.Empty())
-    {
-        isclean[path.Back().first][path.Back().second] = 1;
-        path.PopBack();
-    }
-}
-void onlyclear()
-{
-    while(!path.Empty())
-    {
-        //isclean[path.back().first][path.back().second] = 1;
-        path.PopBack();
-    }
-}
+ 
 void showans()
 {
     fout1.close();
     ifstream fin1("test.path");
-    fout << count << endl;
+    fout << count - 1 << endl;
     for(int i = 0; i < count; i++)
     {
         int m, n;
         fin1 >> m >> n;
-        fout << m << " " << n << endl;
+        fout << m - 1 << " " << n - 1 << endl;
     }
 }
+ 
 bool back_to_charge(mypair curr, int leaststep)
 {
     //findpath(curr, startpos, abs(disBFS[curr.first][curr.second] - disBFS[startpos.first][startpos.second]));
     //cout << disBFS[curr.first][curr.second] - disBFS[startpos.first][startpos.second] << "   ! " << leaststep << endl;
     if((disBFS[curr.first][curr.second] - disBFS[startpos.first][startpos.second]) + 1 < leaststep)
     {
-        onlyclear();
+        path.myClear();
         return true;
     }
     else
     {
-        onlyclear();
+        path.myClear();
         return false;
     }
 }
+ 
 mypair findcloseunclean(mypair curr)
 {
     bool visited[1100][1100] = {0};
-
+ 
     MyQueue<mypair> q;
-
+ 
     q.Push(curr);
     visited[curr.first][curr.second] = true;
     while(!q.Empty())
@@ -771,40 +827,24 @@ int main()
             else
             {
                 house[i][j] = a - '0';
+                if(house[i][j] == 0)
+                    numtoclean++;
             }
         }
     }
     makedisBFS();
-    /*for(int i = 1; i <= row; i++)
-    {
-        for(int j = 1; j <= col; j++)
-        {
-            cout << disBFS[i][j] % 5;
-        }
-        cout << endl;
-    }*/
-    //makedisDFS(0, start_row, start_col);
-
     curr = startpos;
     isclean[startpos.first][startpos.second] = 1;
-    //trueans.PushBack(startpos);
     fout1 << startpos.first << " " << startpos.second << endl;
     count++;
-    //findpath(make_mypair(41, 54),make_mypair(40, 51) , abs(disBFS[40][51] - disBFS[41][54]));
-    while(!check_clean())
+    while(numtoclean != 0)//!check_clean()
     {
         leaststep = maxstep;
         curr = startpos;
-        //cout << "!!!";
-        resetfuck();
         while(back_to_charge(curr, leaststep))
         {
-
-            //cout << "!!";
             int r = curr.first;
             int c = curr.second;
-            isfind = false;
-            resetfuck();
             if(r + 1 <= row && disBFS[r + 1][c] > disBFS[r][c] && house[r + 1][c] != 1 && isclean[r + 1][c] == 0)
             {
                 path.PushBack(curr);
@@ -847,31 +887,31 @@ int main()
             }
             else
             {
+ 
                 mypair tar = findcloseunclean(curr);
+                /*if(curr == startpos)
+                {
+                    tar = suggest_closest;
+                }
+                else
+                {
+                    tar = findcloseunclean(curr);
+                }*/
+ 
                 if(tar == make_mypair(1100, 1100))
                 {
                     break;
                 }
                 findpath(curr, tar);
-                if(path.Size() == 0)
-                {
-                    //cout << "!!";
-                    isfind = false;
-                    resetfuck();
-                    //path.PushBack(curr);
-                    break;
-                }
-
+ 
                 int gostep = path.Size() - 1;
                 //onlyclear();
                 //findpath(tar, startpos, abs(disBFS[startpos.first][startpos.second] - disBFS[tar.first][tar.second]));
                 int backstep = abs(disBFS[startpos.first][startpos.second] - disBFS[tar.first][tar.second]);
-
+ 
                 if(gostep + backstep > leaststep)
                 {
-                    onlyclear();
-                    isfind = false;
-                    resetfuck();
+                    path.myClear();
                     break;
                 }
                 else
@@ -880,14 +920,18 @@ int main()
                     leaststep = leaststep - gostep + 1;
                     //findpath(curr, tar, abs(disBFS[curr.first][curr.second] - disBFS[tar.first][tar.second]));
                 }
-
+ 
+ 
+ 
             }
-
+ 
             curr = make_mypair(path.Back().first, path.Back().second);
+            if(curr == startpos)
+                break;
             leaststep--;
             showpath();
-            clearpath();
-
+            path.myClear();
+ 
             //cout << "!!!" << leaststep << "!!!";
             /*for(int i = 1; i <= row; i++)
             {
@@ -899,10 +943,10 @@ int main()
             }
             cout << endl;//*/
         }
-        resetfuck();
+ 
         findpath(curr, startpos);
         showpath();
-        clearpath();
+        path.myClear();
         /*cout << endl;
         for(int i = 1; i <= row; i++)
         {
@@ -930,5 +974,5 @@ int main()
     //fout << "12334";
     showans();
     return 0;
-
+ 
 }
